81f66f9f975d09b9de1107617179933f2181c237
Riccardo Metere <metere@cbs.mpg.de>
2016-06-07 20:32:28 +0200

    implemented Fourier-based n-dim unwrapping algorithm fixed bug for unspecified carrier frequency in matrix_algebra.PulseExc started separating matrix_algebra tests included fieldshift and susceptibility early tests

--- pymrt/sequences/matrix_algebra.py
+++ pymrt/sequences/matrix_algebra.py
@@ -112,9 +112,9 @@ def superlorentz(x):
 
 # ======================================================================
 # todo: check that the sampling rate is appropriate: 1024 is usually enough
-_SUPERLORENTZ['x'] = np.logspace(-10.0, 1.8, 1024)
-_SUPERLORENTZ['y'] = superlorentz(_SUPERLORENTZ['x'])
-mrb.elapsed('Superlorentz Approx.')
+# _SUPERLORENTZ['x'] = np.logspace(-10.0, 1.8, 1024)
+# _SUPERLORENTZ['y'] = superlorentz(_SUPERLORENTZ['x'])
+# mrb.elapsed('Superlorentz Approx.')
 
 
 # ======================================================================
@@ -308,7 +308,7 @@ def dynamics_operator(
         # w1x, w1y = re(w1), im(w1)  # for symbolic
         w1x, w1y = w1.real, w1.imag
         bloch_core = np.array([
-            [0.0, - w_c, -w1y],
+            [0.0, -w_c, -w1y],
             [w_c, 0.0, w1x],
             [w1y, -w1x, 0.0]])
         if lineshape:
@@ -755,12 +755,12 @@ class SpinModel:
         Base constructor of the spin model class.
 
         Args:
-            s0 (float): magnetization vector magnitude scaling in arb.units
-            mc (ndarray[float]): magnetization concentration ratios in #
-            w0 (ndarray[float]): resonance angular frequencies in rad/s
-            r1 (ndarray[float]): longitudinal relaxation rates in Hz
-            r2 (ndarray[float]): transverse relaxation rates in Hz
-            k (ndarray[float]): pool-pool exchange rate constants in Hz
+            s0 (float): magnetization vector magnitude scaling in arb.units.
+            mc (ndarray[float]): magnetization concentration ratios in #.
+            w0 (ndarray[float]): resonance angular frequencies in rad/s.
+            r1 (ndarray[float]): longitudinal relaxation rates in Hz.
+            r2 (ndarray[float]): transverse relaxation rates in Hz.
+            k (ndarray[float]): pool-pool exchange rate constants in Hz.
 
         Returns:
             None
@@ -1001,7 +1001,7 @@ class PulseExc:
         # so that setting flip angle to 0 does not munge the shape
         self.shape = 'custom'
         self.duration = duration
-        self.w_c = w_c
+        self.w_c = w_c if w_c is not None else 0.0
         self.w1_arr = w1_arr if w1_arr is not None else np.array((1.0,))
         self.num_steps = len(w1_arr)
         self.dt = self.duration / self.num_steps
@@ -1043,13 +1043,12 @@ class PulseExc:
             duration (float): duration of the pulse in s
             flip_angle (float): flip angle of the excitation in deg
             num_steps (int): number of sampling point for the pulse
-            shape (str|unicode): name of the desired pulse shape.
+            shape (basestr): name of the desired pulse shape.
                 Note: a function named '_shape_[SHAPE]' must exist.
                     [normal|cauchy|sinc|cos_sin]
             shape_kwargs (dict|None): keyword arguments for the shape function
-            w_c (float): carrier angular frequency of the pulse excitation in
-                rad/s
-            propagator_mode (str|unicode): calculation mode for the propagator.
+            w_c (float|None): carrier angular frequency of the pulse in rad/s.
+            propagator_mode (basestr|None): calculation mode for the propagator.
                 Note: a function named '_propagator_[MODE]' must exist.
                     [sum|sum_order1|sum_sep|poly|interp|linear|reduced]
             propagator_kwargs (dict|None): keyword arguments for the
@@ -1418,436 +1417,8 @@ class MtFlash(PulseTrain):
 #     ||      Rect pulse
 
 # ======================================================================
-def test_dynamics_operator_symbolic():
-    """
-    Notes: import pi, sin and cos from sympy
-
-    Returns:
-        None
-    """
-    # todo: make it flexible and working
-    w_c, w1 = sym.symbols('w_c w1')
-    m0 = [sym.symbols('m0{}'.format())]
-
-    # 2-pool model
-    spin_model = SpinModel(
-        s0=100,
-        mc=(1.0, 0.152),
-        w0=((w_c,) * 2),
-        r1=(1.8, 1.0),
-        r2=(32.2581, 8.4746e4),
-        k=(0.05,),
-        approx=(None, 'superlorentz_approx'))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-    # 3-pool model
-    spin_model = SpinModel(
-        s0=100,
-        mc=(1.0, 0.152),
-        w0=((w_c,) * 3),
-        r1=(1.8, 1.0, 1.2),
-        r2=(32.2581, 8.4746e4, 30.0),
-        k=(0.05, 0.5, 0.1),
-        approx=(None, 'superlorentz_approx', None))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-    # 4-pool model
-    spin_model = SpinModel(
-        s0=100,
-        mc=(1.0, 0.152),
-        w0=((w_c,) * 4),
-        r1=(1.8, 1.0, 1.2, 2.0),
-        r2=(32.2581, 8.4746e4, 30.0, 60.0),
-        k=(0.05, 0.5, 0.1, 0.001, 0.4, 0.2),
-        approx=(None, 'superlorentz_approx', None, 'gauss'))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-
-# ======================================================================
-def test_dynamics_operator():
-    """
-    Notes: import pi, sin and cos from numpy
-
-    Returns:
-
-    """
-    w_c = GAMMA * B0
-    w1 = 1.0
-
-    # 2-pool model
-    spin_model = SpinModel(
-        s0=100,
-        mc=(1.0, 0.152),
-        w0=((w_c,) * 2),
-        r1=(1.8, 1.0),
-        r2=(32.2581, 8.4746e4),
-        k=(0.3456,),
-        approx=(None, 'superlorentz_approx'))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-    # 3-pool model
-    spin_model = SpinModel(
-        m0=[v * 100.0 for v in (1.0, 0.152, 0.3)],
-        w0=((w_c,) * 3),
-        r1=(1.8, 1.0, 1.2),
-        r2=(32.2581, 8.4746e4, 30.0),
-        k=(0.05, 0.5, 0.1),
-        approx=(None, 'superlorentz_approx', None))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-    # 4-pool model
-    spin_model = SpinModel(
-        m0=[v * 100.0 for v in (1.0, 0.152, 0.3, 0.01)],
-        w0=((w_c,) * 4),
-        r1=(1.8, 1.0, 1.2, 2.0),
-        r2=(32.2581, 8.4746e4, 30.0, 60.0),
-        k=(0.05, 0.5, 0.1, 0.001, 0.4, 0.2),
-        approx=(None, 'superlorentz_approx', None, 'gauss'))
-
-    print(spin_model)
-    print(spin_model.m_eq)
-    print(spin_model.k_op)
-    print(spin_model.l_op)
-    print(dynamics_operator(spin_model, w_c + 10.0, w1))
-
-
-# ======================================================================
-def test_mt_sequence():
-    """
-    Test for the MT sequence.
-    """
-    w_c = GAMMA * B0
-
-    spin_model = SpinModel(
-        s0=100,
-        mc=(1.0, 0.152),
-        w0=((w_c,) * 2),
-        r1=(1.8, 1.0),
-        r2=(32.2581, 8.4746e4),
-        k=(0.3456,),
-        approx=(None, 'superlorentz_approx'))
-
-    num_repetitions = 300
-
-    mt_flash_kernel = PulseList([
-        Delay(10.0e-3),
-        Spoiler(1.0),
-        PulseExc.shaped(40.0e-3, 220.0, 4000, 'gauss', None,
-                        w_c + 50.0, 'poly', {'fit_order': 5}),
-        Delay(20.0e-3),
-        Spoiler(1.0),
-        PulseExc.shaped(10.0e-6, 90.0, 1, 'rect', None),
-        Delay(30.0e-3)],
-        b0=3.0)
-    mt_flash = PulseTrain(mt_flash_kernel, num_repetitions)
-
-    signal = mt_flash.signal(spin_model)
-
-    print(mt_flash)
-    print(mt_flash.propagator(spin_model))
-    print(signal)
-
-
-# ======================================================================
-def test_approx_propagator(
-        spin_model=SpinModel(
-            s0=100,
-            mc=(0.8681, 0.1319),
-            w0=((GAMMA * B0,) * 2),
-            r1=(1.8, 1.0),
-            r2=(32.2581, 8.4746e4),
-            k=(0.3456,),
-            approx=(None, 'superlorentz_approx')),
-        flip_angle=90.0):
-    """
-    Test the approximation of propagators - for speeding up.
-
-    Args:
-        spin_model (SpinModel):
-        flip_angles (float):
-    """
-    w_c = GAMMA * B0
-
-    modes = ['exact']
-    modes += ['linear', 'reduced']
-    # modes += ['sum_simple', 'sum_order1', 'sum_sep', 'reduced']
-    modes += ['poly_{}'.format(order) for order in range(4, 5)]
-    modes += ['interp_{}_{}'.format(mode, num_samples)
-              for mode in ['linear', 'cubic'] for num_samples in range(4, 5)]
-    modes = {
-        'linear': {
-            'num_samples': tuple(range(10, 20, 5))},
-        'interp': {
-            'method': ('linear', 'cubic'),
-            'num_samples': tuple(range(10, 20, 3))},
-        'reduced': {
-            'num_resamples': tuple(range(10, 20, 5))},
-        'poly': {
-            'fit_order': tuple(range(3, 6))}
-    }
-
-    shapes = {
-        'gauss': {},
-        'lorentz': {},
-        'sinc': {},
-        # 'fermi': {},
-        # 'random': {},
-        'cos_sin': {},
-    }
-    exact_p_ops = {}
-    for shape, shape_kwargs in shapes.items():
-        pulse = PulseExc.shaped(
-            40.0e-3, flip_angle, 4000, shape, shape_kwargs, w_c, 'exact', {})
-        exact_p_ops[shape] = pulse.propagator(spin_model)
-
-    for shape, shape_kwargs in shapes.items():
-        for mode, mode_params in modes.items():
-            kwargs_items = [{}]
-            names = mode_params.keys()
-            for values in itertools.product(*[mode_params[i] for i in names]):
-                kwargs_items.append({k: v for k, v in zip(names, values)})
-            for kwargs in kwargs_items:
-                pulse = PulseExc.shaped(
-                    40.0e-3, flip_angle, 4000, shape, shape_kwargs, w_c,
-                    mode, kwargs)
-                begin_time = time.time()
-                p_op = pulse.propagator(spin_model)
-                elapsed = datetime.timedelta(0, time.time() - begin_time)
-                rel_error = np.sum(np.abs(exact_p_ops[shape] - p_op)) / \
-                            np.sum(np.abs(exact_p_ops[shape]))
-                print('{:>8s}, {:>8s}, {:>48s},\t{:.3e}, {}'.format(
-                    shape, mode, str(kwargs), rel_error, elapsed))
-
-
-# ======================================================================
-def test_z_spectrum(
-        spin_model=SpinModel(
-            s0=100,
-            mc=(0.8681, 0.1319),
-            w0=((GAMMA * B0,) * 2),
-            r1=(1.8, 1.0),
-            r2=(32.2581, 8.4746e4),
-            k=(0.3456,),
-            approx=(None, 'superlorentz_approx')),
-        freqs=np.round(mrb.sgnlogspace(50, 50000, 16)),
-        amplitudes=np.round(mrb.sgnlogspace(50, 5000, 16)),
-        plot_data=True,
-        save_file=None):
-    """
-    Test calculation of z-spectra
-
-    Args:
-
-        spin_model (SpinModel):
-        freqs (ndarray[float]):
-        amplitudes (ndarray[float]):
-        plot_data (bool):
-        save_file (string):
-
-    Returns:
-        freq
-
-    """
-    w_c = spin_model.w0[0]
-
-    flip_angles = amplitudes * 11.799 / 50.0
-
-    mt_flash = MtFlash(
-        PulseList([
-            Delay(10.0e-3),
-            Spoiler(1.0),
-            PulseExc.shaped(10.0e-3, 90.0, 4000, 'gauss', {},
-                            0.0, 'poly', {'fit_order': 3}),
-            Delay(10.0e-3),
-            Spoiler(1.0),
-            PulseExc.shaped(2.1e-3, 11.0, 1, 'rect', {})],
-            w_c=w_c),
-        300)
-
-    data = np.zeros((len(freqs), len(amplitudes)))
-    for j, freq in enumerate(freqs):
-        for i, flip_angle in enumerate(flip_angles):
-            mt_flash.set_flip_angle(flip_angle)
-            mt_flash.set_freq(freq)
-            data[j, i] = mt_flash.signal(spin_model)
-
-    # plot results
-    if plot_data:
-        X, Y = np.meshgrid(amplitudes, np.log10(freqs))
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.plot_surface(
-            X, Y, data, cmap=plt.cm.hot,
-            rstride=1, cstride=1, linewidth=0.01, antialiased=False)
-    if save_file:
-        np.savez(save_file, freqs, amplitudes, data)
-    return data, freqs, flip_angles
-
-
-# ======================================================================
-def test_fit_spin_model(
-        snr_level=20,
-        plot_data=True):
-    """
-    Test calculation of z-spectra
-
-    Args:
-        snr_level (float):
-        plot_data (bool):
-
-    Returns:
-        None
-    """
-    w_c = GAMMA * B0
-
-    # mt_flash = MtFlash(
-    #     PulseList([
-    #         Delay(16610.0e-6),
-    #         Spoiler(0.0),
-    #         Delay(160.0e-6),
-    #         PulseExc.shaped(10000.0e-6, 90.0, 0, '_from_GAUSS5120', {},
-    #                         0.0, 'poly', {'fit_order': 3}),
-    #         Delay(160.0e-6 + 970.0e-6),
-    #         Spoiler(1.0),
-    #         Delay(160.0e-6),
-    #         PulseExc.shaped(100e-6, 11.0, 1, 'rect', {}),
-    #         Delay(4900.0e-6)],
-    #         w_c=w_c),
-    #     300)
-
-    t_e = 1.7e-3
-    t_r = 70.0e-3
-    w_c = 297220696
-    mt_flash = MtFlash(
-        PulseList([
-            Delay(
-                t_r - (t_e + 3 * 160.0e-6 + 20000.0e-6 + 970.0e-6 + 100e-6)),
-            Spoiler(0.0),
-            Delay(160.0e-6),
-            PulseExc.shaped(
-                20000.0e-6, 90.0, 0, '_from_GAUSS5120', {}, 0.0,
-                'linear', {'num_samples': 15}),
-            Delay(160.0e-6 + 970.0e-6),
-            Spoiler(1.0),
-            Delay(160.0e-6),
-            PulseExc.shaped(100e-6, 30.0, 1, 'rect', {}),
-            Delay(t_e)],
-            w_c=w_c),
-        num_repetitions=100 * 100)
-
-    def mt_signal(x_arr, s0, mc_a, r1a, r2a, r2b, k_ab):
-        spin_model = SpinModel(
-            s0=s0,
-            mc=(mc_a, 1.0 - mc_a),
-            w0=(w_c, w_c * (1 - 3.5e-6)),
-            # w0=((w_c,) * 2),
-            r1=(r1a, 1.0),
-            r2=(r2a, r2b),
-            k=(k_ab,),
-            approx=(None, 'superlorentz_approx'))
-        y_arr = np.zeros_like(x_arr[:, 0])
-        i = 0
-        for freq, flip_angle in x_arr:
-            mt_flash.set_flip_angle(flip_angle)
-            mt_flash.set_freq(freq)
-            y_arr[i] = mt_flash.signal(spin_model)
-            i += 1
-        return y_arr
-
-    # simulate a measurement
-    freqs = mrb.sgnlogspace(100.0, 300.0e3, 32)
-    flip_angles = np.linspace(1.0, 1100.0, 32)
-
-    x_data = np.array(tuple(itertools.product(freqs, flip_angles)))
-
-    # see: mt_signal
-    p_e = 100, 0.8681, 2.0, 32.2581, 8.4746e4, 0.3456
-    exact = mt_signal(x_data, *p_e).reshape((len(freqs), len(flip_angles)))
-    # num = len(freqs) * len(flip_angles)
-    # noise = (np.random.rand(*exact.shape) - 0.5) * np.max(exact) / snr_level
-    # measured = exact + noise
-    #
-    # p0 = 100, 0.5, 5.0, 20.0, 5e4, 0.5
-    # bounds = [[50, 1000], [0, 1], [0.1, 10], [10, 50], [1e4, 1e5], [0, 1]]
-    # y_data = measured.ravel()
-    #
-    # def sum_of_squares(params, x_data, m_data):
-    #     e_data = mt_signal(x_data, *params)
-    #     return np.sum((m_data - e_data) ** 2.0)
-    #
-    # res = scipy.optimize.minimize(
-    #     sum_of_squares, p0, args=(x_data, y_data), method='L-BFGS-B',
-    #     bounds=bounds, options={'gtol': 1e-05, 'ftol': 2e-09})
-    # print(res.x, res.success, res.message)
-    #
-    # fitted = mt_signal(x_data, *res.x).reshape(measured.shape)
-
-    # # faked fitted
-    # fitted = mt_signal(x_data, *p0).reshape(measured.shape)
-
-    if plot_data:
-        X, Y = np.meshgrid(flip_angles, mrb.sgnlog(freqs, 10.0))
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        ax.set_xlabel('Pulse Amplitude (flip angle) / deg')
-        ax.set_ylabel('Frequency offset / Hz (log10 scale)')
-        ax.set_zlabel('Signal Intensity / arb.units')
-        ax.plot_surface(
-            X, Y, exact, cmap=plt.cm.hot,
-            rstride=1, cstride=1, linewidth=0.005, antialiased=False)
-        # ax.plot_surface(
-        #     X, Y, measured, cmap=plt.cm.ocean,
-        #     rstride=1, cstride=1, linewidth=0.01, antialiased=False)
-        # ax.plot_surface(
-        #     X, Y, fitted, cmap=plt.cm.bone,
-        #     rstride=1, cstride=1, linewidth=0.01, antialiased=False)
-
-
-# ======================================================================
 if __name__ == '__main__':
     print(__doc__)
-    # test_dynamics_operator_symbolic()
-    # mrb.elapsed'test_symbolic')
-    # test_dynamics_operator()
-    # mrb.elapsed'test_dynamics_operator')
-    # test_mt_sequence()
-    # mrb.elapsed'test_mt_sequence')
-    # test_approx_propagator()
-    # mrb.elapsed'test_approx_propagator')
-    # test_z_spectrum(
-    #     SpinModel(100.0, (0.5, 0.3, 0.1, 0.1), (GAMMA * B0,) * 4,
-    #               (0.25, 0.8, 0.001, 1.0), (20.0, 60.0, 8e4, 5e4),
-    #               (1.0, 0.3, 0.0, 1.0, 0.5, 1.0),
-    #               (None, None, 'superlorenz_approx', 'superlorenz_approx')))
-    # test_z_spectrum()
-    # mrb.elapsed'test_z_spectrum')
-    test_fit_spin_model()
-    mrb.elapsed('test_fit_spin_model')
 
     mrb.print_elapsed()
     # profile.run('test_z_spectrum()', sort=1)
---
+++ sandbox/suscept2fieldshift.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Calculate the z-component field perturbation due to susceptibility variation.
+
+Consider a sample with a certain susceptibility spatial distribution chi(r)
+(r is the position vector) submerged in a large z-oriented constant magnetic
+field B0_z of known intensity. Assume chi(r) << 1.
+The field shift DB_z(r) due to the susceptibility distribution can be easily
+calculated in the Fourier domain:
+DB_z(k) = B0_z chi(k) C(k)
+where:
+- chi(k) is the Fourier transform of chi(r)
+- C(k) is the Fourier transform of the dipole convolution kernel
+The C-factor is dependent upon orientation relative to B0z in the Fourier
+space. Such relation can be expressed more conveniently as a function of two
+subsequent rotations in the direct space:
+- a rotation of angle th (theta) around x-axis
+- a rotation of angle ph (phi) around y-axis
+(It assumed that x-, y- and z- axes are on the 1st, 2nd and 3rd dimensions of
+the volume.)
+The full expression is given by:
+              ( 1   (kz cos(th) cos(ph) - ky sin(th) cos(ph) + kx sin(ph))^2 )
+ C(k,th,ph) = ( - - ==================================================------ )
+              ( 3                      kx^2 + ky^2 + kz^2                    )
+The resulting field shift is then given by:
+DB_z(r) = DB_z(r) / B0_z
+
+[ref: S. Wharton, R. Bowtell, NeuroImage 53 (2010) 515-525]
+[ref: J.P. Marques, R. Bowtell, Concepts in MR B 25B (2005) 65-78]
+"""
+
+
+# ======================================================================
+# :: Future Imports
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+__version__ = '0.0.0.13'
+# $Source$
+
+
+# ======================================================================
+# :: Custom Module Details
+AUTHOR = 'Riccardo Metere'
+CONTACT = 'metere@cbs.mpg.de'
+DATE_INFO = {'day': 18, 'month': 'Sep', 'year': 2014}
+DATE = ' '.join([str(v) for k, v in sorted(DATE_INFO.items())])
+LICENSE = 'License GPLv3: GNU General Public License version 3'
+COPYRIGHT = 'Copyright (C) ' + str(DATE_INFO['year'])
+# first non-empty line of __doc__
+DOC_FIRSTLINE = [line for line in __doc__.splitlines() if line][0]
+
+
+# ======================================================================
+# :: Python Standard Library Imports
+# import os  # Miscellaneous operating system interfaces
+# import shutil  # High-level file operations
+# import math  # Mathematical functions
+import time  # Time access and conversions
+import datetime  # Basic date and time types
+# import operator  # Standard operators as functions
+import collections  # High-performance container datatypes
+import argparse  # Parser for command-line options, arguments and sub-commands
+# import itertools  # Functions creating iterators for efficient looping
+# import subprocess  # Subprocess management
+# import multiprocessing  # Process-based parallelism
+# import csv  # CSV File Reading and Writing [CSV: Comma-Separated Values]
+# import json  # JSON encoder and decoder [JSON: JavaScript Object Notation]
+
+# :: External Imports
+import numpy as np  # NumPy (multidimensional numerical arrays library)
+# import scipy as sp  # SciPy (signal and image processing library)
+# import matplotlib as mpl  # Matplotlib (2D/3D plotting library)
+# import sympy as sym  # SymPy (symbolic CAS library)
+# import PIL  # Python Image Library (image manipulation toolkit)
+# import SimpleITK as sitk  # Image ToolKit Wrapper
+# import nibabel as nib  # NiBabel (NeuroImaging I/O Library)
+# import nipy  # NiPy (NeuroImaging in Python)
+# import nipype  # NiPype (NiPy Pipelines and Interfaces)
+
+# :: External Imports Submodules
+import matplotlib.pyplot as plt  # Matplotlib's pyplot: MATLAB-like syntax
+# import mayavi.mlab as mlab  # Mayavi's mlab: MATLAB-like syntax
+# import scipy.optimize  # SciPy: Optimization Algorithms
+# import scipy.integrate  # SciPy: Integrations facilities
+# import scipy.constants  # SciPy: Mathematal and Physical Constants
+# import scipy.ndimage  # SciPy: ND-image Manipulation
+
+# :: Local Imports
+# import mr_lib.base as mrb
+# import mr_lib.utils as mru
+import mr_lib.nifti as mrn
+# import mr_lib.geom_mask as mrgm
+# import mr_lib.mp2rage as mp2rage
+
+
+# ======================================================================
+# :: supported verbosity levels (level 4 skipped on purpose)
+VERB_LVL = {'none': 0, 'low': 1, 'medium': 2, 'high': 3, 'debug': 5}
+D_VERB_LVL = VERB_LVL['low']
+
+
+# ======================================================================
+def suscept2fieldshift(
+        chi_r,
+        b0z,
+        theta,
+        phi):
+    """
+    Calculate the z-comp. field perturbation due to susceptibility variation.
+
+    Parameters
+    ==========
+    chi_r : ndarray
+        Input voxmap containing susceptibility distribution chi(r) in SI units
+    bb0z : float
+        Main magnetic field B0 in Tesla
+    theta : float
+        Angle of 1st rotation (along x-axis) in deg
+    phi : float
+        Angle of 2nd rotation (along y-axis) in deg
+
+    Returns
+    =======
+    delta_bdz_r : numpy 3D-array
+        Output voxmap containing z-component of field shift  DBdz(r) over B0_z
+
+    """
+
+    # convert input angles to radians
+    theta = np.deg2rad(theta)
+    phi = np.deg2rad(phi)
+    # susceptibility in Fourier space (zero-centered)
+    chi_k = np.fft.fftshift(np.fft.fftn(chi_r))
+    # :: dipole convolution kernel C = C(k, theta, phi)
+    #     / 1   (kz cos(th) cos(ph) - ky sin(th) cos(ph) + kx sin(ph))^2 \
+    # C = | - - ==================================================------ |
+    #     \ 3                      kx^2 + ky^2 + kz^2                    /
+    k_range = [slice(-k_size / 2.0, +k_size / 2.0) for k_size in chi_k.shape]
+    kk_x, kk_y, kk_z = np.ogrid[k_range]
+    dipole_kernel_k = (1.0 / 3.0 -
+        (kk_z * np.cos(theta) * np.cos(phi)
+        - kk_y * np.sin(theta) * np.cos(phi)
+        + kk_x * np.sin(phi)) ** 2 /
+        (kk_x ** 2 + kk_y ** 2 + kk_z ** 2))
+    # fix singularity for |k|^2 == 0 with DBdz(k) = B0z * chi(k) / 3
+    singularity = np.isnan(dipole_kernel_k)
+    dipole_kernel_k[singularity] = 1.0 / 3.0
+    # field shift along z in Tesla in Fourier space
+    delta_bdz_k = b0z * chi_k * dipole_kernel_k
+    # field shift along z in Tesla in direct space (zero-centered)
+    delta_bdz_r = np.fft.ifftn(np.fft.ifftshift(delta_bdz_k))
+    delta_bdz_r = np.real(delta_bdz_r) / b0z
+    return delta_bdz_r
+
+
+# ======================================================================
+def handle_arg():
+    """
+    Handle command-line application arguments.
+    """
+    # :: Define DEFAULT values
+    # verbosity
+    d_verbose = D_VERB_LVL
+    # main magnetic field B0
+    d_b0 = 7.0  # T
+    # polar angle
+    d_theta = 0.0  # deg
+    # azimuthal angle
+    d_phi = 0.0  # deg
+    # input file
+    d_input_file = 'tmp_suscept.nii.gz'
+    # output file
+    d_output_file = 'tmp_fieldshift.nii.gz'
+    # :: Create Argument Parser
+    arg_parser = argparse.ArgumentParser(
+        description=__doc__,
+        epilog='v.{} - {} {} <{}>\n{}'.format(
+            __version__, COPYRIGHT, AUTHOR, CONTACT, LICENSE),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    # :: Add POSIX standard arguments
+    arg_parser.add_argument(
+        '--ver', '--version',
+        version='%(prog)s {}\n{}\n{} {} <{}>\n{}'.format(
+            __version__, DOC_FIRSTLINE, COPYRIGHT, AUTHOR, CONTACT, LICENSE),
+        action='version')
+    arg_parser.add_argument(
+        '-v', '--verbose',
+        action='count', default=d_verbose,
+        help='increase the level of verbosity [%(default)s]')
+    # :: Add additional arguments
+    arg_parser.add_argument(
+        '-b', '--b0z', metavar='B0_z',
+        type=float, default=d_b0,
+        help='set the magnetic field B0 along z-axis in Tesla [%(default)s]',)
+    arg_parser.add_argument(
+        '-t', '--theta', metavar='TH',
+        type=float, default=d_theta,
+        help='set angle of 1st rotation (along x-axis) in deg [%(default)s]',)
+    arg_parser.add_argument(
+        '-p', '--phi', metavar='PH',
+        type=float, default=d_phi,
+        help='set angle of 2nd rotation (along y-axis) in deg [%(default)s]',)
+    arg_parser.add_argument(
+        '-i', '--input', metavar='FILE',
+        default=d_input_file,
+        help='set input file [%(default)s]',)
+    arg_parser.add_argument(
+        '-o', '--output', metavar='FILE',
+        default=d_output_file,
+        help='set output file [%(default)s]',)
+    return arg_parser
+
+
+# ======================================================================
+if __name__ == '__main__':
+    # handle program parameters
+    ARG_PARSER = handle_arg()
+    ARGS = ARG_PARSER.parse_args()
+    # print calling arguments
+    if ARGS.verbose >= VERB_LVL['debug']:
+        print(ARGS)
+    # perform calculation
+    begin_time = time.time()
+    mrn.simple_filter(
+        ARGS.input, ARGS.output,
+        suscept2fieldshift, [ARGS.b0z, ARGS.theta, ARGS.phi])
+    end_time = time.time()
+    if ARGS.verbose > VERB_LVL['none']:
+        print('TotTime:\t', datetime.timedelta(0, end_time - begin_time))
