1e14c1afdc6fde65478a1c7248f5e376dd3b6e3c
Riccardo Metere <metere@cbs.mpg.de>
2017-11-01 17:44:07 +0100

    ENH: auto convert function for MATLAB files.

--- pymrt/extras/matlab.py
+++ pymrt/extras/matlab.py
@@ -16,6 +16,7 @@ from __future__ import (
 # ======================================================================
 # :: Python Standard Library Imports
 import os  # Miscellaneous operating system interfaces
+import warnings  # Warning control
 
 # :: External Imports
 import numpy as np  # NumPy (multidimensional numerical arrays library)
@@ -27,6 +28,8 @@ import scipy.io  # SciPy: Input and output
 
 # :: Local Imports
 import pymrt as mrt
+import pymrt.utils
+import pymrt.input_output
 from pymrt import VERB_LVL, D_VERB_LVL, VERB_LVL_NAMES
 from pymrt import msg, dbg
 
@@ -72,7 +75,7 @@ def read(
         data = {
             k: np.array(v) for k, v in data.items() if not exclude_keys(k)}
         msg('Loaded using MATLAB v4 l1, v6, v7 <= v7.2 compatibility layer.',
-            VERB_LVL['debug'])
+            verbose, VERB_LVL['debug'])
     except NotImplementedError:
         # load new-style MATLAB v7.3+ files (effectively HDF5)
         data = {}
@@ -81,7 +84,7 @@ def read(
             if not exclude_keys(k):
                 data[k] = np.array(v)
         msg('Loaded using MATLAB v7.3+ compatibility layer.',
-            VERB_LVL['debug'])
+            verbose, VERB_LVL['debug'])
     return data
 
 
@@ -89,7 +92,8 @@ def read(
 def write(
         data,
         filename,
-        dirpath='.'):
+        dirpath='.',
+        verbose=D_VERB_LVL):
     """
     Write a MATLAB file.
 
@@ -103,9 +107,6 @@ def write(
 
     Returns:
         None.
-
-    Examples:
-
     """
     if not filename.lower().endswith('.mat'):
         filename += '.mat'
@@ -113,7 +114,70 @@ def write(
 
     sp.io.savemat(filepath, data)
     msg('Saved using MATLAB v4 l1, v6, v7 <= v7.2 compatibility layer.',
-        VERB_LVL['debug'])
+        verbose, VERB_LVL['debug'])
+
+
+# ======================================================================
+def auto_convert(
+        filepaths,
+        out_template='{basepath}__{name}.nii.gz',
+        save_kws=None,
+        on_exist='unique',
+        verbose=D_VERB_LVL):
+    """
+    Automatically convert MATLAB data to PyMRT's standard format (NIfTI).
+
+    Args:
+        filepaths (Iterable[str]): List of filepaths to convert.
+            Can be easily combined with file listing functions from `utils`,
+            e.g. `iflistdir()`, `flistdir()`, `listdir()` (directly) or
+            `iwalk2()`, `walk2()` (indirectly).
+        out_template (str): Template for the output filepath.
+            The following variables are available for interpolation:
+             - `dirpath`: The directory of the input.
+             - `base`: The input base file name without extension.
+             - `ext`: The input file extension (with leading separator).
+             - `basepath`: The input filepath without extension.
+             - `name`: The name of the variable as stored in the MATLAB file.
+        save_kws (dict|None): Keyword parameters for `input_output.save()`.
+        on_exist (str): Determine what to do if output exists.
+            Accepted values are:
+             - 'unique': Generate a new unique filepath.
+             - 'skip': Skip saving the data.
+             - 'overwrite': Overwrites the data onto existing filepath!
+        verbose (int): Set level of verbosity.
+
+    Returns:
+        None.
+    """
+    if save_kws is None:
+        save_kws = {}
+    on_exist = on_exist.lower()
+    for in_filepath in filepaths:
+        if os.path.isfile(in_filepath):
+            msg('Loading: {}'.format(in_filepath), verbose, D_VERB_LVL)
+            for name, arr in read(in_filepath, verbose=verbose).items():
+                dirpath, base, ext = mrt.utils.split_path(in_filepath)
+                basepath = os.path.join(dirpath, base)
+                out_filepath = out_template.format_map(locals())
+                do_save = True
+                if os.path.exists(out_filepath):
+                    if on_exist == 'skip':
+                        do_save = False
+                    elif on_exist == 'overwrite':
+                        pass
+                    else:
+                        if on_exist != 'unique':
+                            text = 'Unknown value `{}` for `on_exist`. ' \
+                                   'Default to `unique`.'
+                            warnings.warn(text)
+                        out_filepath = mrt.utils.next_filepath(out_filepath)
+
+                if do_save:
+                    msg('Saving: {}'.format(out_filepath), verbose, D_VERB_LVL)
+                    mrt.input_output.save(out_filepath, arr, **save_kws)
+                else:
+                    msg('Skip: {}'.format(out_filepath), verbose, D_VERB_LVL)
 
 
 # ======================================================================
