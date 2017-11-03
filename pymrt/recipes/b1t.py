FIX: more for units for dimensionless quantity.

pymrt/recipes/b1t.py
@@ -65,7 +65,7 @@ def afi(
             Contains the signal with :math:`T_{R,1}`.
         arr2 (np.ndarray): The second input array in arb.units.
             Contains the signal with :math:`T_{R,2}`.
-        n_tr (int|float): The repetition times ratio in #.
+        n_tr (int|float): The repetition times ratio in one units.
             This is defined as :math:`n = \\frac{T_{R,2}}{T_{R,1}}`.
         fa (int|float): The flip angle in deg.
         prepare (callable|None): Input array preparation.
@@ -74,7 +74,7 @@ def afi(
             correction of magnitude data from Rician mean bias.
 
     Returns:
-        eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
             This is the :math:`\\eta_\\alpha` factor.
 
     References:
@@ -144,7 +144,7 @@ def double_rare(
             correction of magnitude data from Rician mean bias.
 
     Returns:
-        eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
             This is the :math:`\\eta_\\alpha` factor.
 
     Notes:
@@ -246,7 +246,7 @@ def double_flash(
             correction of magnitude data from Rician mean bias.
 
     Returns:
-        eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
             This is the :math:`\\eta_\\alpha` factor.
     """
     arr1 = prepare(arr1) if prepare else arr1.astype(float)
@@ -363,7 +363,7 @@ def mp2rage_rho(
              `sequences.mp2rage.rho()`.
 
     Returns:
-        eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
 
     References:
         1) Marques, J.P., Kober, T., Krueger, G., van der Zwaag, W.,

pymrt/recipes/multi_flash.py
@@ -57,7 +57,7 @@ def _flash_signal(
             Contains information on the spin density `m0`, the coil
             sensitivity (proportional to `b1r`) and units transformation
             factors.
-        eta_fa (int|float|np.ndarray): The flip angle efficiency in #.
+        eta_fa (int|float|np.ndarray): The flip angle efficiency in one units.
 
     Returns:
         s_arr (np.ndarray): The signal array in arb.units.
@@ -89,7 +89,7 @@ def _flash_signal_fit(
             Contains information on the spin density `m0`, the coil
             sensitivity (proportional to `b1r`) and units transformation
             factors.
-        eta_fa (float): The flip angle efficiency in #.
+        eta_fa (float): The flip angle efficiency in one units.
 
     Returns:
         s_arr (np.ndarray): The signal array in arb.units.
@@ -165,7 +165,7 @@ def triple_special1(
         tr (int|float): The base repetition time in time units.
             This is the repetition time for `arr1` and `arr2`, while the
             repetition time for `arr3` must be `n * tr`.
-        n_tr (int|float): The repetition times ratio in #.
+        n_tr (int|float): The repetition times ratio in one units.
             This is the repetition times ratio, obtained dividing the
             repetition time of `arr3` by the repetition time of `arr1`.
         sign (int|float): Select one of the two solution for the equations.
@@ -182,7 +182,7 @@ def triple_special1(
                The units of `t1_arr` are defined by the units of `tr`.
              - xi_arr (np.ndarray): The signal factor in arb. units.
                This is :math:`\\eta_{m_0} m_0 e^{-\\frac{T_E}{T_2^*}`.
-             - eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+             - eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
                This is proportional to the coil transmit field :math:`B_1^+`.
     """
     fa = np.deg2rad(fa)
@@ -301,7 +301,7 @@ def triple_special2(
         tr (int|float): The base repetition time in time units.
             This is the repetition time for `arr1` and `arr2`, while the
             repetition time for `arr3` must be `n * tr`.
-        n_tr (int|float): The repetition times ratio in #.
+        n_tr (int|float): The repetition times ratio in one units.
             This is the repetition times ratio, obtained dividing the
             repetition time of `arr3` by the repetition time of `arr1`.
         sign (int|float): Select one of the two solution for the equations.
@@ -323,7 +323,7 @@ def triple_special2(
                The units of `t1_arr` are defined by the units of `tr`.
              - xi_arr (np.ndarray): The signal factor in arb. units.
                This is :math:`\\eta_{m_0} m_0 e^{-\\frac{T_E}{T_2^*}`.
-             - eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+             - eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
                This is proportional to the coil transmit field :math:`B_1^+`.
     """
     fa = np.deg2rad(fa)
@@ -449,7 +449,7 @@ def triple_approx(
                The units of `t1_arr` are defined by the units of `tr`.
              - xi_arr (np.ndarray): The signal factor in arb. units.
                This is :math:`\\eta_{m_0} m_0 e^{-\\frac{T_E}{T_2^*}`.
-             - eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+             - eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
                This is proportional to the coil transmit field :math:`B_1^+`.
     """
     fa1 = np.deg2rad(fa1)
@@ -544,7 +544,7 @@ def vfa(
         arrs (Iterable[np.ndarray]): The input signal arrays in arb.units
         fas (Iterable[int|float]): The flip angles in deg.
         trs (Iterable[int|float]): The repetition times in time units.
-        eta_fa_arr (np.ndarray|None): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray|None): The flip angle efficiency in one units.
             If None, a significant bias may still be present.
         prepare (callable|None): Input array preparation.
             Must have the signature: f(np.ndarray) -> np.ndarray.
@@ -700,7 +700,7 @@ def fit_leasq(
                The units of `t1_arr` are defined by the units of `tr`.
              - xi_arr (np.ndarray): The signal factor in arb. units.
                This is :math:`\\eta_{m_0} m_0 e^{-\\frac{T_E}{T_2^*}`.
-             - eta_fa_arr (np.ndarray): The flip angle efficiency in #.
+             - eta_fa_arr (np.ndarray): The flip angle efficiency in one units.
                This is proportional to the coil transmit field :math:`B_1^+`.
     """
     assert (len(arrs) == len(fas) == len(trs))

pymrt/recipes/t1.py
@@ -60,7 +60,7 @@ def mp2rage_rho(
             used and the values must be in the (-0.5, 0.5) range.
             If `use_ratio` is True, the ratio s1/s2 is used and the values are
             not bound.    
-        eta_fa_arr (int|float|np.array): Flip angle efficiency in #.
+        eta_fa_arr (int|float|np.array): Flip angle efficiency in one units.
             This is equivalent to the normalized B1T field.
             If np.ndarray, it must have the same shape as `rho_arr`.
             If int or float, the flip angle efficiency is assumed to be
@@ -219,7 +219,7 @@ def double_flash(
         fa2 (int|float): The second nominal flip angle in deg.
         tr1 (int|float): The first repetition time in time units.
         tr2 (int|float): The second repetition time in time units.
-        eta_fa_arr (int|float|np.ndarray): The flip angle efficiency in #.
+        eta_fa_arr (int|float|np.ndarray): The flip angle efficiency in one units.
             If int or float, it is assumed constant throught the inputs.
             If np.ndarray, its shape must match the shape of both `arr1` and
             `arr2`.
@@ -311,7 +311,7 @@ def multi_flash(
         arrs (Iterable[np.ndarray]): The input signal arrays in arb.units
         fas (Iterable[int|float]): The flip angles in deg.
         trs (Iterable[int|float]): The repetition times in time units.
-        eta_fa_arr (np.ndarray|None): The flip angle efficiency in #.
+        eta_fa_arr (np.ndarray|None): The flip angle efficiency in one units.
             If None, a significant bias may still be present.
         method (str): Determine the fitting method to use.
             Accepted values are:

pymrt/sequences/flash.py
@@ -111,9 +111,9 @@ def signal(
             Units must be the same as `t2s`.
         t2s (int|float|np.ndarray): The transverse relaxation in time units.
             Units must be the same as `te`.
-        eta_fa (int|float|np.ndarray): The flip angle efficiency in #.
+        eta_fa (int|float|np.ndarray): The flip angle efficiency in one units.
             This is proportional to the coil transmit field :math:`B_1^+`.
-        eta_m0 (int|float|np.ndarray): The spin density efficiency in #.
+        eta_m0 (int|float|np.ndarray): The spin density efficiency in one units.
             This is proportional to the coil receive field :math:`B_1^-`.
 
     Returns:

pymrt/sequences/mp2rage.py
@@ -277,8 +277,8 @@ def rho(
         td2 (float|np.ndarray): Third delay in ms.
             This is the time between the end of the second GRE block and the
             beginning of the preparation pulse.
-        fa_p (float|np.ndarray): Flip angle of the preparation pulse in deg.
-        eta_p (float|np.ndarray): Efficiency of the preparation pulse in #.
+        fa_p (float|np.ndarray): Flip angle of the preparation in deg.
+        eta_p (float|np.ndarray): Efficiency of the preparation in one units.
         t1 (float|np.ndarray): Longitudinal relaxation time in ms.
         eta_fa (float|np.ndarray): Efficiency of the RF excitation.
             This only affects the excitation inside the GRE blocks.