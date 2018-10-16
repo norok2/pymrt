24b46589dc39cd3fc0b94ac25698c9894591d74b
Riccardo Metere <rick@metere.it>
2018-10-16 08:17:51 +0200

    WIP: Numerically safe trajectories.

--- pymrt/sequences/trajectories.py
+++ pymrt/sequences/trajectories.py
@@ -1,29 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-pymrt.geometry: create and manipulate 2D, 3D and N-D geometries.
+pymrt.sequences.trajectories: create and manipulate 2D, 3D and N-D trajectories
 
-The 2D geometrical shapes currently available are:
-- square
-- rectangle
-- rhombus
-- circle
-- ellipse
-
-The 3D geometrical shapes currently available are:
-- cube
-- cuboid
-- rhomboid
-- sphere
-- ellipsoid
-- cylinder
-
-The N-D geometrical shapes currently available are:
-- cuboid: sum[abs(x_n/a_n)^inf] < 1
-- superellipsoid: sum[abs(x_n/a_n)^k] < 1
-- prism: stack (N-1)-D rendered objects on given axis
-
-Running this file directly will run some tests.
+This is useful for pulse sequences evaluation.
 """
 
 # ======================================================================
@@ -89,7 +69,7 @@ def zig_zag_cartesian_2d(
         blip_size=1,
         num_trains=None,
         num_blips=None,
-        train_step=1):
+        step=1):
     """
     Generate a zig-zag cartesian trajectory.
 
@@ -98,7 +78,7 @@ def zig_zag_cartesian_2d(
         blip_size:
         num_trains:
         num_blips:
-        train_step:
+        step:
 
     Returns:
         result (tuple): The tuple
@@ -113,15 +93,15 @@ def zig_zag_cartesian_2d(
         num_trains = num_blips + 1
     elif not num_blips:
         num_blips = num_trains - 1
-    train = np.arange(0, abs(train_size), train_step) * np.sign(train_size)
-    blip = np.arange(1, abs(blip_size), train_step) * np.sign(blip_size)
+    train = np.arange(0, abs(train_size), step) * np.sign(train_size)
+    blip = np.arange(1, abs(blip_size), step) * np.sign(blip_size)
     x, y, mask = [], [], []
     y_offset = 0
     for j in range(num_trains):
-        t = train[slice(None, None, -1) if j % 2 else slice(None)]
-        x.extend(t.tolist())
-        y.extend((np.zeros_like(t) + y_offset).tolist())
-        mask.extend(np.ones_like(t, dtype=bool).tolist())
+        slicing = slice(None, None, -1) if j % 2 else slice(None, None, 1)
+        x.extend(train[slicing].tolist())
+        y.extend((np.zeros_like(train) + y_offset).tolist())
+        mask.extend(np.ones_like(train, dtype=bool).tolist())
         if j < num_blips:
             x.extend([train[0 if j % 2 else -1]] * blip.size)
             y.extend((blip + y_offset).tolist())
@@ -134,8 +114,7 @@ def zig_zag_cartesian_2d(
 def zig_zag_linear_2d(
         train_size,
         num_trains,
-        normal_size=1,
-        train_step=1):
+        step=1):
     """
     Generate a zig-zag linear trajectory.
 
@@ -144,7 +123,7 @@ def zig_zag_linear_2d(
         normal_size:
         num_trains:
         num_blips:
-        train_step:
+        step:
 
     Returns:
         result (tuple): The tuple
@@ -152,35 +131,27 @@ def zig_zag_linear_2d(
              - x_i (np.ndarray): The coordinates of the trajectory.
              - mask (np.ndarray): The mask for the zig-zag trains.
     """
-    raise NotImplementedError
-    train = np.arange(0, abs(train_size), train_step) * np.sign(train_size)
-    blip = np.arange(1, abs(normal_size), train_step) * np.sign(normal_size)
-    x, y, mask = [], [], []
-    y_offset = 0
+    num_points = (abs(train_size) - 1) * abs(num_trains) + 1
+    train = np.arange(0, abs(train_size), step) * np.sign(train_size)
+    x = [0]
     for j in range(num_trains):
-        t = train[slice(None, None, -1) if j % 2 else slice(None)]
-        x.extend(t.tolist())
-        y.extend((np.zeros_like(t) + y_offset).tolist())
-        mask.extend(np.ones_like(t, dtype=bool).tolist())
-        if j < num_blips:
-            x.extend([train[0 if j % 2 else -1]] * blip.size)
-            y.extend((blip + y_offset).tolist())
-            mask.extend(np.zeros_like(blip, dtype=bool).tolist())
-        y_offset += normal_size
-    return np.array([x, y]), mask
+        slicing = slice(-2, None, -1) if j % 2 else slice(1, None, 1)
+        x.extend(train[slicing].tolist())
+    y = np.arange(0, num_points, step)
+    mask = np.ones(num_points, dtype=bool)
+    return np.stack([x, y]), mask
 
 
 import matplotlib.pyplot as plt
 
 traj_up, train_mask = zig_zag_cartesian_2d(11, 2, num_blips=4)
-traj_dn, train_mask = zig_zag_linear_2d(10, -5, num_blips=4)
+traj_dn, train_mask = zig_zag_linear_2d(-11, -5)
+
+print(traj_dn.shape)
+print(traj_dn)
 
 traj_up = reframe(traj_up, bounds=5)
-traj_dn = reframe(traj_dn, bounds=((-0.5, 2), (0, 1)))
-
-print(traj_up)
-print(train_mask)
-print(traj_up.shape)
+traj_dn = reframe(traj_dn, bounds=5)
 
 fig, ax = plt.subplots()
 ax.axis('equal')
