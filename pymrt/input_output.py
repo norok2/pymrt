FIX: Update to use `fc.num.apply_at()` and better masking

pymrt/input_output.py
@@ -1049,12 +1049,9 @@ def calc_stats(
         utils.calc_stats
     """
     arr = load(arr_filepath)
-    if mask_filepath:
-        obj_mask = nib.load(mask_filepath)
-        mask = obj_mask.get_data().astype(bool)
-    else:
-        mask = slice(None)
-    return fc.num.calc_stats(arr[mask], *args, **kwargs)
+    mask = load(mask_filepath).astype(bool) if mask_filepath else None
+    return fc.num.calc_stats(
+        arr[mask] if mask is None else arr, *args, **kwargs)
 
 
 # ======================================================================
@@ -1207,12 +1204,9 @@ def plot_histogram1d(
     """
     obj = nib.load(in_filepath)
     arr = obj.get_data().astype(np.double)
-    if mask_filepath:
-        obj_mask = nib.load(mask_filepath)
-        mask = obj_mask.get_data().astype(bool)
-    else:
-        mask = slice(None)
-    result = mrt.plot.histogram1d(arr[mask], *args, **kwargs)
+    mask = load(mask_filepath).astype(bool) if mask_filepath else None
+    result = mrt.plot.histogram1d(
+        arr[mask] if mask is not None else arr, *args, **kwargs)
     return result
 
 
@@ -1239,16 +1233,12 @@ def plot_histogram1d_list(
     See Also:
         plot
     """
-    if mask_filepath:
-        obj_mask = nib.load(mask_filepath)
-        mask = obj_mask.get_data().astype(bool)
-    else:
-        mask = slice(None)
+    mask = load(mask_filepath).astype(bool) if mask_filepath else None
     arr_list = []
     for in_filepath in in_filepaths:
         obj = nib.load(in_filepath)
         arr = obj.get_data()
-        arr_list.append(arr[mask])
+        arr_list.append(arr[mask] if mask is not None else arr)
     result = mrt.plot.histogram1d_list(arr_list, *args, **kwargs)
     return result
 

pymrt/recipes/chi.py
@@ -266,8 +266,7 @@ def db0_to_chi(
 
     # threshold the zeros of the dipole kernel
     if threshold:
-        mask = np.abs(dk) < threshold
-        dk[mask] = threshold
+        dk[np.abs(dk) < threshold] = threshold
 
     # divide the magnetic field variation by the dipole kernel
     chi_k_arr = np.fft.fftn(db0_arr) / dk
@@ -582,8 +581,7 @@ def qsm_field2source_l2_closed_form(
 
     # threshold the zeros of the dipole kernel
     if threshold:
-        mask = np.abs(dk) < threshold
-        dk[mask] = threshold
+        dk[np.abs(dk) < threshold] = threshold
 
     # compute the gradient operators along all dims
     exp_2_k = sum(

pymrt/recipes/generic.py
@@ -356,11 +356,8 @@ def referencing(
 # ======================================================================
 def _pre_exp_loglin(arr, exp_factor=0, zero_cutoff=np.spacing(1.0)):
     arr = np.abs(arr)
-    log_arr = np.zeros_like(arr)
-    # calculate logarithm only of strictly positive values
-    mask = tuple(slice(None) for d in arr.shape) \
-        if zero_cutoff is None else np.abs(arr) > zero_cutoff
-    log_arr[mask] = (np.log(arr[mask] * np.exp(exp_factor)))
+    log_arr = fc.num.apply_at(
+        arr, lambda x: np.log(x) / np.exp(exp_factor), arr > zero_cutoff)
     return log_arr
 
 

pymrt/recipes/phs.py
@@ -464,13 +464,12 @@ def unwrap_laplacian(
     kk2 = fftshift(fc.num.laplace_kernel(arr.shape, factors=arr.shape))
     arr = fftn(cos_arr * ifftn(kk2 * fftn(sin_arr)) -
                sin_arr * ifftn(kk2 * fftn(cos_arr)))
-    kk2[kk2 != 0] = 1.0 / kk2[kk2 != 0]
+    fc.num.apply_at(kk2, lambda x: 1 / x, kk2 != 0, in_place=True)
     arr *= kk2
     del cos_arr, sin_arr, kk2
     arr = np.real(ifftn(arr))
 
-    arr = arr[mask]
-    return arr
+    return arr[mask]
 
 
 # ======================================================================
@@ -575,7 +574,7 @@ def unwrap_gradient(
     for kk, grad in zip(kks, grads):
         u_arr += -1j * kk * fftn(np.real(-1j * grad / arr))
         kk2 += kk ** 2
-    kk2[kk2 != 0] = 1.0 / kk2[kk2 != 0]
+    fc.num.apply_at(kk2, lambda x: 1 / x, kk2 != 0, in_place=True)
     arr = np.real(ifftn(kk2 * u_arr)) / (2 * np.pi)
     return arr[mask]
 